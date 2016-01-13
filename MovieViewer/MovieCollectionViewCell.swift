5bc720a1eba1e97d2c215ed50d291d241aee554c
Edward Xue <edthemaster1@gmail.com>
2016-01-12 22:55:29 -0500

    Added in basic collection view (separate from the tableview for now).

---
+++ MovieViewer/MovieCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieCollectionViewCell.swift
+//  MovieViewer
+//
+//  Created by Edward Xue on 1/12/16.
+//  Copyright © 2016 eidehua. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
