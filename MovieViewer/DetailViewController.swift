67983aba5aeaadb74787cf38c0fd30ec3916b517
Edward Xue <edthemaster1@gmail.com>
2016-01-20 20:16:00 -0600

    Added Scrolling for detail view

--- MovieViewer/DetailViewController.swift
+++ MovieViewer/DetailViewController.swift
@@ -13,11 +13,17 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var infoView: UIView!
     
     var movie: NSDictionary!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = CGSize(width: scrollView.frame.size.width, height: infoView.frame.origin.y + infoView.frame.size.height)
+        print(infoView.frame.origin.y)
+        print(scrollView.frame.size.width)
         
         // Do any additional setup after loading the view.
         let title = movie["title"] as? String
@@ -25,6 +31,7 @@ class DetailViewController: UIViewController {
         
         let overview = movie["overview"] as? String
         overviewLabel.text = overview
+        overviewLabel.sizeToFit()
         
         let baseUrl = "http://image.tmdb.org/t/p/w500"
         if let posterPath = movie["poster_path"] as? String {
